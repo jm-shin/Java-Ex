169e3cc5ece6ffd054326eb5e46e6f62fbf6bbdd
jongminshin <ace4463@gmail.com>
2020-05-05 21:34:05 +0900

    first commit

---
+++ ch6/TvTest.java
@@ -0,0 +1,20 @@
+class Tv{
+    String color;
+    boolean power;
+    int channel;
+
+    //Tv기능
+    void power(){power = !power;}
+    void channelUp(){++channel;}
+    void channelDown(){--channel;}
+}
+
+class TvTest{
+    public static void main(String[] args) {
+        Tv t;
+        t = new Tv();
+        t.channel = 7;
+        t.channelDown();
+        System.out.println("현재 채널은 "+t.channel+"입니다.");
+    }
+}
